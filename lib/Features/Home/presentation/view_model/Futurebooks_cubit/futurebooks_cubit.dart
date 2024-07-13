43e081559f0aa4e2c68fc895f4ea06d326df624d
ahmed-algarbani <ahmedalgarbani776@gmail.com>
2024-07-13 14:49:55 +0300

    create cubit home Futurebookscubit method and emit

--- lib/Features/Home/presentation/view_model/Futurebooks_cubit/futurebooks_cubit.dart
+++ lib/Features/Home/presentation/view_model/Futurebooks_cubit/futurebooks_cubit.dart
@@ -1,9 +1,22 @@
+import 'package:Bookly/Features/Home/Data/models/book_model/Repo/home_repo.dart';
 import 'package:Bookly/Features/Home/Data/models/book_model/book.dart';
+import 'package:Bookly/core/errors/failure.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'futurebooks_state.dart';
 
 class FuturebooksCubit extends Cubit<FuturebooksState> {
-  FuturebooksCubit() : super(FuturebooksInitial());
+  FuturebooksCubit(this.homeRepo) : super(FuturebooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> featchFeaturedBooks() async {
+    emit(FuturebooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(FuturebooksFailure(failure.errorMessage));
+    }, (books) {
+      emit(FuturebooksSussecc(books));
+    });
+  }
 }
