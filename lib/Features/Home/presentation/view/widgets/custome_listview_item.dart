53f98938fe4fe356da031a40eb7466779e49dc74
ahmed-algarbani <ahmedalgarbani776@gmail.com>
2024-07-21 10:59:19 +0300

    fix chache image network pachage

--- lib/Features/Home/presentation/view/widgets/custome_listview_item.dart
+++ lib/Features/Home/presentation/view/widgets/custome_listview_item.dart
@@ -1,5 +1,6 @@
 import 'package:Bookly/constant.dart';
 import 'package:Bookly/core/app_route.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,19 +10,16 @@ class CustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .23,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      // height: MediaQuery.of(context).size.height * .23,
       child: AspectRatio(
         aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.all(Radius.circular(10)),
-            image: DecorationImage(
-              image: NetworkImage(ImageUrl),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
+        child: CachedNetworkImage(imageUrl: ImageUrl,fit: BoxFit.fill
+        ,
+        placeholder: (context, url) => Center(child: CircularProgressIndicator(),),
+        errorWidget: (context, url, error) => Icon(Icons.error),
+        )
       ),
     );
   }
